b1966a95dd2bbbcf51538e5440dc64d621534781
Dave Jarvis <davej@dmrassociates.com>
2015-07-28 17:57:45 -0500

    Update search radius global. General code cleanup

--- SocialRadar/CustomClass/SearchViewController.m
+++ SocialRadar/CustomClass/SearchViewController.m
@@ -91,135 +91,18 @@
     [searchBarMap resignFirstResponder];
  
     
-  /* 
-   //Commented in order to use foursquare api for showing the searched location list//
-   NSString *lat=[NSString stringWithFormat:@"%f",appDelegate.currentlocation.coordinate.latitude];
-    NSString *longt=[NSString stringWithFormat:@"%f",appDelegate.currentlocation.coordinate.longitude];
-    
-    NSString *searchTwo = [searchBar.text stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
-    //comment out the below 2line
-    //NSString *str=[@"%" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    //searchTwo=[NSString stringWithFormat:@"%@%@",str,searchTwo];
-    //[NSString stringWithFormat:@"'%@'",searchTwo]
-    
-    NSURL *myURL1 = [NSURL URLWithString:[NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/textsearch/json?location=%@,%@&radius=35000&query=%@&sensor=true&key=AIzaSyAfXbpScWudV9n79teEzazPNkhqGZjjVJA",lat,longt,[NSString stringWithFormat:@"'%@'",searchTwo] ]];
-    //[ img setImageWithURL:myURL];
-    
-    NSURLRequest* urlRequest = [NSURLRequest requestWithURL:myURL1];
-    NSData* data = [NSURLConnection sendSynchronousRequest:urlRequest
-                                         returningResponse:nil error:nil];
-    NSString* strTemp = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    SBJSON* json = [[SBJSON alloc] init];
-    NSDictionary* dict = [NSDictionary dictionaryWithDictionary:[json objectWithString:strTemp]];
-
-    if ([[dict valueForKey:@"status"]isEqualToString:@"OVER_QUERY_LIMIT"]) {
-        
-        [searchBarMap resignFirstResponder];
-        UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"Google Map response exceeded the daily limit." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-         errorAlert.tag=1001;
-        [errorAlert show];
-        _RELEASE(errorAlert);
-    }
-    else if ([[dict valueForKey:@"status"]isEqualToString:@"ZERO_RESULTS"]){
-        UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"No result found." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        [errorAlert show];
-        _RELEASE(errorAlert);
-
-    }
-    else{
-        NSMutableArray *arrayResult = [dict valueForKey:@"results"];
-        
-        NSMutableArray *array = [[NSMutableArray alloc] init];
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains[c] %@",searchBar.text]; // if you need case sensitive search avoid '[c]' in the predicate
-        for (int i =0 ; i < [arrayResult count]; i++)
-        {
-            NSDictionary *dict=[arrayResult objectAtIndex:i];
-            //Matching the search location
-            if ([predicate evaluateWithObject:[dict valueForKey:@"name"]]) {
-                NearByLocation *nearByLocation = [[NearByLocation alloc] initWithDict:[arrayResult objectAtIndex:i]];
-                [array addObject:nearByLocation];
-                [nearByLocation release];
-            }
-            
-            
-        }
-        
-        SearchResultView *search = [[SearchResultView alloc] initWithNibName:@"SearchResultView" bundle:nil];
-        search.arrayOfTextSearch = [array retain];
-        [self.navigationController pushViewController:search animated:YES];
-        [search release];
-        
-        [array release];
-        array = nil;
-    }*/
-       
     
 }
 
 -(void)callFourSquareLocations:(NSString *)searchText
 {
-    //Search within the 21 miles radius from the current location.
+    //Search within the _SEARCHVIEW_SEARCH_RADIUS  from the current location.
     
     NSString *lat=[NSString stringWithFormat:@"%f",appDelegate.currentlocation.coordinate.latitude];
     NSString *longt=[NSString stringWithFormat:@"%f",appDelegate.currentlocation.coordinate.longitude];
     
-    /*//Showing location list using Foursquare API
-    [Foursquare2 searchVenuesNearByLatitude:[NSNumber numberWithFloat:[lat floatValue]] longitude:[NSNumber numberWithFloat:[longt floatValue]]
-                                 accuracyLL:nil
-                                   altitude:nil
-                                accuracyAlt:nil
-                                      query:searchText
-                                      limit:[NSNumber numberWithInteger:10]
-                                     intent:intentCheckin
-                                     radius:@(35000)
-                                 categoryId:nil
-                                   callback:^(BOOL success, id result){
-                                       if (success)
-                                       {
-                                           NSDictionary *jsonObject = result;
-                                           NSMutableArray *arrayResult = [jsonObject valueForKeyPath:@"response.venues"];
-                                           NSMutableArray *mainArray = [[NSMutableArray alloc] init];
-                                           NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains[c] %@",searchText]; // if you need case sensitive search avoid '[c]' in the predicate
-                                           for (int i =0 ; i < [arrayResult count]; i++)
-                                           {
-                                               if ([predicate evaluateWithObject:[[arrayResult objectAtIndex:i] valueForKey:@"name"]])
-                                               {
-                                                   NearByLocation *nearByLocation = [[NearByLocation alloc] initWithDict:[arrayResult objectAtIndex:i]];
-                                                   [mainArray addObject:nearByLocation];
-                                                   [nearByLocation release];
-                                                   
-                                               }
-                                           }
-                                           if (mainArray.count) {
-                                               SearchResultView *search = [[SearchResultView alloc] initWithNibName:@"SearchResultView" bundle:nil];
-                                               search.arrayOfTextSearch = [mainArray retain];
-                                               [self.navigationController pushViewController:search animated:YES];
-                                               [search release];
-                                               
-                                               [mainArray release];
-                                               mainArray = nil;
-                                           }
-                                           else{
-                                               UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"No result found." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-                                               errorAlert.tag=1002;
-                                               [errorAlert show];
-                                               _RELEASE(errorAlert);
-                                           
-                                           }
-         
-                                        }
-                                       else{
-                                           
-                                           UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"No result found." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-                                           errorAlert.tag=1002;
-                                           [errorAlert show];
-                                           _RELEASE(errorAlert);
-                                       }
-                                   }];
-    //end of showling location list using Foursquare API*/
-    
     //New Foursquare Trending API
-   [Foursquare2 venueSearchNearByLatitude:[NSNumber numberWithFloat:[lat floatValue]] longitude:[NSNumber numberWithFloat:[longt floatValue]] query:searchText limit:[NSNumber numberWithInteger:50] intent:intentBrowse radius:@(19312.08) categoryId:nil callback:^(BOOL success, id result)
+   [Foursquare2 venueSearchNearByLatitude:[NSNumber numberWithFloat:[lat floatValue]] longitude:[NSNumber numberWithFloat:[longt floatValue]] query:searchText limit:[NSNumber numberWithInteger:50] intent:intentBrowse radius:@(_SEARCHVIEW_SEARCH_RADIUS) categoryId:nil callback:^(BOOL success, id result)
     {
         if (success)
         {
@@ -265,66 +148,6 @@
         }
 
     }];
-    //End of  Foursquare Trending API
-    
-    //Venue/Explore
-   
-    /*[Foursquare2 venueExploreRecommendedNearByLatitude:[NSNumber numberWithFloat:[lat floatValue]] longitude:[NSNumber numberWithFloat:[longt floatValue]] near:nil accuracyLL:[NSNumber numberWithFloat:10000.0] altitude:[NSNumber numberWithInt:0] accuracyAlt:[NSNumber numberWithFloat:10000.0] query:nil limit:[NSNumber numberWithInt:50] offset:nil radius:[NSNumber numberWithFloat:19312.08] section:nil novelty:nil sortByDistance:NO openNow:NO venuePhotos:NO price:nil callback:^(BOOL success, id result){
-            
-        NSDictionary *jsonObject = result;
-        NSMutableArray *venues2 = [jsonObject valueForKeyPath:@"response.groups.items"];
-        NSMutableArray *venues=[venues2 objectAtIndex:0];
-        if (success) {
-            NSMutableArray *arrayResult = [venues2 objectAtIndex:0];
-            NSMutableArray *mainArray = [[NSMutableArray alloc] init];
-            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains[c] %@",searchText]; // if you need case sensitive search avoid '[c]' in the predicate
-            for (int i =0 ; i < [arrayResult count]; i++)
-            {
-                if ([predicate evaluateWithObject:[[[arrayResult objectAtIndex:i] valueForKey:@"venue"] valueForKey:@"name"]])
-                {
-                    NSMutableDictionary *catDict=[[[[venues objectAtIndex:i] objectForKey:@"venue"] objectForKey:@"categories"] objectAtIndex:0];
-                    NSString *prefix= [[catDict objectForKey:@"icon"] valueForKey:@"prefix"];
-                    if ([prefix rangeOfString:@"outdoors"].location == NSNotFound) {
-                        NearByLocation *nearByLocation = [[NearByLocation alloc] initWithDict:[[venues objectAtIndex:i] objectForKey:@"venue"]];
-                        [mainArray addObject:nearByLocation];
-                        [nearByLocation release];
-                    }
-                    
-                  
-                    
-                }
-            }
-            if (mainArray.count) {
-                SearchResultView *search = [[SearchResultView alloc] initWithNibName:@"SearchResultView" bundle:nil];
-                search.arrayOfTextSearch = [mainArray retain];
-                [self.navigationController pushViewController:search animated:YES];
-                [search release];
-                
-                [mainArray release];
-                mainArray = nil;
-            }
-            else{
-                UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"No result found." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-                errorAlert.tag=1002;
-                [errorAlert show];
-                _RELEASE(errorAlert);
-                
-            }
-
-        
-        }
-        else{
-            UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:@"" message:@"No result found." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-            errorAlert.tag=1002;
-            [errorAlert show];
-            _RELEASE(errorAlert);
-        }
-       
-    }];*/
-
-    
-    
-    
     
     
     [self performSelector:@selector(stopAnimatingIndicatorView) withObject:nil afterDelay:4];
